eb80a3037813c3b1de07b124dbfd4750897d4f8b
DitgerDW <ditger.de.wolf@hotmail.com>
2020-07-22 14:11:46 +0200

    Setting up starting point for other pages

--- events.php
+++ events.php
@@ -1,2 +1,11 @@
 <?php include "includes/header.php" ?>
+<div class="container">
+    <div class="text-center">
+        <h1>
+            Events die je niet wil missen
+        </h1>
+
+        <p class="status">We rusten even uit na heel wat event-gymnastiek.</p>
+    </div>
+</div>
 <?php include "includes/footer.php" ?>
--- manifesto.php
+++ manifesto.php
@@ -1,2 +1,140 @@
 <?php include "includes/header.php" ?>
+    <div class="container">
+        <p class="jumbotron">
+            De Creativity Gym is niet zomaar een turnzaal omgebouwd tot een coole co-working omgeving. Het is een manier
+            van denken en met elkaar omgaan die we binnen onze opleiding Interactive Multimedia Design nastreven.
+            Kom je werken in de Gym, neem dan volgende spelregels in acht. Onze gym crew kan je diskwalificeren indien
+            je de spelregels overtreedt.
+        </p>
+
+        <div class="manifesto d-flex flex-column ">
+            <div class="row odd align-items-start">
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+            </div>
+            <div class="row even align-items-end">
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+            </div>
+
+            <div class="row odd">
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+            </div>
+            <div class="row even">
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+            </div>
+
+            <div class="row odd">
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+            </div>
+            <div class="row even">
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+            </div>
+
+            <div class="row odd">
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+            </div>
+            <div class="row even">
+                <div class="col-md-3">
+                    <span>1</span>
+                </div>
+                <div class="col-md-3">
+                    <div class="oddTitle">
+                        Respect yourself. Respect the gym.
+                    </div>
+
+                    <div class="oddText">
+                        De gym werd gebouwd door drie docenten ( @GoodBytes, @RobbyVanelderen en @Davidheerinckx ) van de opleiding Interactive Multimedia Design tijden de vakantieperiode. Samen met de hulp van een aantal enorm hulpvaardige IMD studenten en collega's kon een leegstaande turnzaal omgebouwd worden tot wat de zaal nu is.
+                        Respecteer de gym zoals je je eigen materiaal respecteert.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php include "includes/footer.php" ?>
